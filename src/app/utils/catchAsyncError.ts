3048580d8c764510de47120c5f25d340418b4954
shanta_islam <shanta@etlimited.net>
2024-06-30 11:40:33 +0600

    validation control and error control

---
+++ src/app/utils/catchAsyncError.ts
@@ -0,0 +1,8 @@
+import { NextFunction, Request, RequestHandler, Response } from "express";
+
+const catchAsyncError = (fn: RequestHandler) => {
+    return (req: Request, res: Response, next: NextFunction) => {
+      Promise.resolve(fn(req, res, next)).catch((err) => next(err));
+    };
+  };
+export default catchAsyncError;  
