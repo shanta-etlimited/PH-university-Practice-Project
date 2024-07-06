dca2a0c9c47a2d87f4edf300b72218220362a04a
shanta_islam <shanta@etlimited.net>
2024-07-06 18:49:25 +0600

    add transaction and rollback

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,53 +1,68 @@
-import httpStatus from "http-status";
-import config from "../../config";
-import AppError from "../../errors/AppError";
-import { AcademicSemester } from "../academicSemester/academicSemester.model";
-import { TStudent } from "../student/student.interface";
-import { Student } from "../student/student.model";
-import { TUser } from "./user.interface";
-import { User } from "./user.model";
-import { generateStudentId } from "./user.utils";
+import httpStatus from 'http-status';
+import config from '../../config';
+import AppError from '../../errors/AppError';
+import { AcademicSemester } from '../academicSemester/academicSemester.model';
+import { TStudent } from '../student/student.interface';
+import { Student } from '../student/student.model';
+import { TUser } from './user.interface';
+import { User } from './user.model';
+import { generateStudentId } from './user.utils';
+import mongoose from 'mongoose';
 
 const createStudentIntoDB = async (password: string, payload: TStudent) => {
- 
-    //create a user object
-    let userData: Partial<TUser>={};
+  //create a user object
+  let userData: Partial<TUser> = {};
 
-    //if password is not given, use default password
-    if(!password){
-        userData.password = config.default_password as string
-    }
-    else{
-        userData.password = password
-    }
-    
-    //set student role
-    userData.role = "student"  
+  //if password is not given, use default password
+  if (!password) {
+    userData.password = config.default_password as string;
+  } else {
+    userData.password = password;
+  }
 
-    const admissionSemester = await AcademicSemester.findById(payload.admissionSemester)
+  //set student role
+  userData.role = 'student';
 
-    if (admissionSemester === null) {
-        // Handle the case when the document is not found
-        throw new AppError(httpStatus.NOT_FOUND, 'Admission semester not found');
-    }
+  const admissionSemester = await AcademicSemester.findById(
+    payload.admissionSemester,
+  );
 
+  if (admissionSemester === null) {
+    // Handle the case when the document is not found
+    throw new AppError(httpStatus.NOT_FOUND, 'Admission semester not found');
+  }
+
+  const session = await mongoose.startSession();
+
+  try {
+    session.startTransaction();
     //set manually generated id
-    userData.id = await generateStudentId(admissionSemester)
+    userData.id = await generateStudentId(admissionSemester);
 
-    //create a user
-    const newUser  = await User.create(userData);
+    //create a user(transaction-1)
+    const newUser = await User.create([userData], { session });
     //create a student
-    if(Object.keys(newUser).length){
-        //set id , _id as user
-        payload.id = newUser.id;
-        payload.user = newUser._id
-
-        const newStudent = await Student.create(payload);
-        return newStudent;
+    if (!newUser.length) {
+      throw new AppError(httpStatus.BAD_REQUEST, 'Failed to create user');
     }
+      //set id , _id as user
+      payload.id = newUser[0].id;
+      payload.user = newUser[0]._id; //reference _id
 
-}
+    //create a student(transaction-2)
+      const newStudent = await Student.create([payload], { session });
+      if(!newStudent.length){
+        throw new AppError(httpStatus.BAD_REQUEST, 'Failed to create student');
+      }
+      await session.commitTransaction();
+      await session.endSession();
+      return newStudent;
+  } catch (err) {
+    await session.abortTransaction();
+    await session.endSession();
+  }
+};
 
 export const UserServices = {
-    createStudentIntoDB
-}
+  createStudentIntoDB,
+};
