96b7a84efdf2303c731a8546df8fd172a15a9237
shanta_islam <shanta@etlimited.net>
2024-07-11 13:56:26 +0600

    create gender, bloodgroup constant

--- src/app/modules/student/student.constant.ts
+++ src/app/modules/student/student.constant.ts
@@ -1,5 +1,19 @@
+import { TBloodGroup, TGender } from "./student.interface";
+
+export const Gender: TGender[] = ['male', 'female', 'other'];
+
+export const BloodGroup: TBloodGroup[] = [
+  'A+',
+  'A-',
+  'B+',
+  'B-',
+  'AB+',
+  'AB-',
+  'O+',
+  'O-',
+];
 export const studentSearchableFields = [
     'email',
     'name.firstName',
     'presentAddress',
-  ];
+];
